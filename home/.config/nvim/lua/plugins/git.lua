5e6f0e721b6f12a855edf8363183a73fa7386cc1
pragyann <stha.pragyan.17@gmail.com>
2026-08-15 21:03:09 +1000

    add key bind to close git diffview

--- home/.config/nvim/lua/plugins/git.lua
+++ home/.config/nvim/lua/plugins/git.lua
@@ -5,6 +5,18 @@ return {
         keys = { { '<leader>g', function() require('neogit').open() end, desc = 'Neogit' } },
     },
     {
+        -- installed as a neogit dependency; configured here for the keymaps
+        'sindrets/diffview.nvim',
+        opts = {
+            keymaps = {
+                -- buffer-local, so q still records macros everywhere else
+                view = { { 'n', 'q', '<cmd>DiffviewClose<cr>', { desc = 'Close Diffview' } } },
+                file_panel = { { 'n', 'q', '<cmd>DiffviewClose<cr>', { desc = 'Close Diffview' } } },
+                file_history_panel = { { 'n', 'q', '<cmd>DiffviewClose<cr>', { desc = 'Close Diffview' } } },
+            },
+        },
+    },
+    {
         'lewis6991/gitsigns.nvim',
         event = 'BufWinEnter',
         opts = { current_line_blame = true }, -- who last touched this line
