f25f0ce697ceeaeaebc331bc7da5153f02acb3c0
pragyann <stha.pragyan.17@gmail.com>
2026-08-15 20:43:56 +1000

    setup telescope

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,43 @@
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
+    'nvim-telescope/telescope.nvim',
+    version = '*',
     dependencies = {
         'nvim-lua/plenary.nvim',
         -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     config = function()
+        local telescope = require('telescope')
+
+        telescope.setup({
+            defaults = {
+                -- telescope's default rg args plus --hidden, so dotfiles and
+                -- dotfolders are searched. the first six flags produce the
+                -- file:line:col: output telescope parses, so they must stay.
+                vimgrep_arguments = {
+                    'rg',
+                    '--color=never',
+                    '--no-heading',
+                    '--with-filename',
+                    '--line-number',
+                    '--column',
+                    '--smart-case',
+                    '--hidden',
+                    '--glob=!**/.git/*',
+                },
+            },
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    -- lua patterns, not globs
+                    file_ignore_patterns = { '^%.git/', '/%.git/' },
+                },
+            },
+        })
+
+        -- native C sorter; falls back to the lua sorter if the build is missing
+        pcall(telescope.load_extension, 'fzf')
+
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
